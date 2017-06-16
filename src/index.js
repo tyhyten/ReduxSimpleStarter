6769e25f861628163d3346fcdcef3090c2f57633
Ty Hyten <ty.hyten@acumendigital.com>
2017-06-16 15:28:28 -0600

    Complete section 2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import _ from 'lodash';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-const API_KEY = 'AIzaSyCf3QZZ71rkZtG9WIJJy3Jvv-ih3c94cWI';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+const API_KEY = 'AIzaSyDo10ZXI5ChgZR2QiVIIpn62EwrCcVSi_E';
 
 // create new component that should produce HTMl
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surfboards')
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  };
+
+
+
+  render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos}/>
+      </div>
+    );
+  }
+}
+;
 
 // Put generated HTML in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
